9e1c48b1abd2bbb2ab3e05e0fb00cce2eb7a2f6b
ZEROOCPT <105050106+ZEROOCPT@users.noreply.github.com>
2023-07-25 16:45:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Dan inilah cara Kamu mengganti nama file dari variabel!
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+sampah_organik = ["daun","buah",'makanan']
+sampah_anorganik = ['plastik','stereofoam','cabel']
+
+@bot.command()
+async def pilih(ctx):
+    while True:
+        await ctx.send("masukan sampah")
+        msg = await bot.wait_for("message")
+
+        if msg.content in sampah_organik :
+            await ctx.send("masukkan dalam organik")
+        elif msg.content in sampah_anorganik :
+            await ctx.send("masukkan dalam anorganik") 
+        else :
+            await ctx.send("entri tidak ada maka program berhenti") 
+            break
+
+
+bot.run("MTExNzc1ODg0NzE4NTA1OTkxMA.GAVefI.v81foX5HmvAMGN_X2TFJyhOVeF8oOrBJgOag9A")
